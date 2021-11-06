2b1749840fb9885a02facb894d6203fb0f3080d4
Avi711 <avraham711@gmail.com>
2021-11-06 21:58:06 +0200

    Adding names.

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -9,10 +9,13 @@ void SimpleAnomalyDetector ::learnNormal(const TimeSeries &ts) {
 
     for (int i = 1; i < data.size(); ++i) {
         int m = 0;
+        int c = -1;
+        for (int j = i; j < data.size(); ++j) {
+            int** ar = new int* [5];
+            int** ar2 = new int*[5];
 
 
-
-
+        }
     }
 
 
