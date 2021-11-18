0e3bf84cf9fe4f2d0845fde3d0e4edaf2cab6e63
kehat <keatsudri@gmail.com>
2021-11-18 19:02:54 +0200

    comments for SIMPLE ANOMALY DETECTOR

--- SimpleAnomalyDetector.h
+++ SimpleAnomalyDetector.h
@@ -14,27 +14,56 @@
 #include <string.h>
 #include <math.h>
 
-struct correlatedFeatures{
-    string feature1,feature2;  // names of the correlated features
+/*
+ * this struct represent couple of correlated features
+ * we save their names , their corrlation (we calculate) ,  (in float) their linear regression (we calculate) ,
+ * and their threshold (above this values the features have corrlation together)
+ */
+struct correlatedFeatures {
+    string feature1, feature2;  // names of the correlated features
     float corrlation;
     Line lin_reg;
     float threshold;
 };
 
-class SimpleAnomalyDetector:public TimeSeriesAnomalyDetector{
+/*
+ * This class is actually Simple Anomaly Detector extend from Time Series Anomaly Detector
+ * Time series is our data we work on , we have here vector of correlated Features
+ * and functions to create and run our Simple Anomaly Detector
+ */
+class SimpleAnomalyDetector : public TimeSeriesAnomalyDetector {
     vector<correlatedFeatures> cf;
 public:
+    //constructor
     SimpleAnomalyDetector();
+
+    //distructor
     virtual ~SimpleAnomalyDetector();
-    virtual void learnNormal(const TimeSeries& ts);
-    virtual vector<AnomalyReport> detect(const TimeSeries& ts);
-    vector<correlatedFeatures> getNormalModel(){
+
+    /*
+     * Here we find the correlative pairs (columns of time series)
+     */
+    virtual void learnNormal(const TimeSeries &ts);
+
+    /*
+     * here we detect the anomalies and insert them into vector
+     */
+    virtual vector<AnomalyReport> detect(const TimeSeries &ts);
+
+    /*For the testability of the department. In this method we return
+     * List of correlative properties as described in the code. Thus, we can check that learning is normal
+     */
+    vector<correlatedFeatures> getNormalModel() {
         return cf;
     }
-    void findMaxDev(const TimeSeries&ts);
 
-    };
+    /*
+     * find the max deviation between point and the line equation of the points
+     * its help us to set the threshold.
+     */
+    void findMaxDev(const TimeSeries &ts);
 
+};
 
 
 #endif /* SIMPLEANOMALYDETECTOR_H_ */
