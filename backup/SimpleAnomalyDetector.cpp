d38b5c7742d253e911e05f7fb7019996cabe5c94
Avi711 <avraham711@gmail.com>
2021-12-14 20:17:32 +0200

    adding

---
+++ backup/SimpleAnomalyDetector.cpp
@@ -0,0 +1,98 @@
+//
+// Created by avi on 05/11/2021.
+//
+
+#include "SimpleAnomalyDetector.h"
+
+SimpleAnomalyDetector::SimpleAnomalyDetector() {}
+
+SimpleAnomalyDetector::~SimpleAnomalyDetector() noexcept {}
+
+void SimpleAnomalyDetector::findMaxDev(const TimeSeries &ts) {
+    float trs = 0;
+    for (int i = 0; i < this->cf.size(); ++i) {
+        if (cf[i].corrlation >= cf[i].threshold) {
+            for (int j = 0; j < ts.getValuesByName(cf[i].feature1).size(); ++j) {
+                float x = ts.getValuesByName(cf[i].feature1)[j];
+                float y = ts.getValuesByName(cf[i].feature2)[j];
+                Point *p = new Point(x, y);
+                trs = dev(*p, cf[i].lin_reg);
+                if (trs > cf[i].threshold) {
+                    cf[i].threshold = (trs) * 1.2;
+                }
+            }
+        }
+    }
+
+}
+
+void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
+    std::vector<Feature> data = ts.getData2();
+    vector<Point *> points;
+    for (int i = 0; i < data.size(); ++i) {
+        int m = 0;
+        int c = -1;
+        float sum = 0, counter = 0;
+        for (int j = i; j < data.size(); ++j) {
+            if (i == j) continue;
+            float p = pearson(&data[i].getValues()[0], &data[j].getValues()[0], data[j].getValues().size());
+            sum += fabs(p);
+            counter++;
+            if (std::fabs(p) > m) {
+                m = p;
+                c = j;
+            }
+            if (c != -1) {
+                correlatedFeatures cf1;
+                cf1.feature1 = data[i].getName();
+                cf1.feature2 = data[j].getName();
+                cf1.corrlation = fabs(p);
+                cf1.threshold = 0;
+                for (int k = 0; k < data[i].getValues().size(); ++k) {
+                    Point *p = new Point(data[i].getValue(k), data[j].getValue(k));
+                    points.push_back(p);
+                }
+                cf1.lin_reg = linear_reg(&points[0], data[i].getValues().size());
+                points.clear();
+                this->cf.push_back(cf1);
+            }
+        }
+        float avg = sum / counter;
+        for (int m = 0; m < cf.size(); m++) {
+            if (cf[m].corrlation < 0.9) cf.erase(cf.begin() + m);
+        }
+    }
+    findMaxDev(ts);
+
+}
+
+vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries &ts) {
+    vector<Point *> points;
+    std::vector<AnomalyReport> reports;
+    /*for (int i = 0; i < this->cf.size(); ++i) {
+        if (cf[i].corrlation >= cf[i].threshold) {
+            for (int j = 0; j < ts.getValuesByName(cf[i].feature1).size(); ++j) {
+                float x = ts.getValuesByName(cf[i].feature1)[j];
+                float y = ts.getValuesByName(cf[i].feature2)[j];
+                Point *p = new Point(x, y);
+                points.push_back(p);
+            }
+        }
+        cf[i].lin_reg = linear_reg(&points[0], points.size());
+        points.clear();
+    }*/
+    for (int i = 0; i < this->cf.size(); ++i) {
+      //  if (cf[i].corrlation >= cf[i].threshold) {
+            for (int j = 0; j < ts.getValuesByName(cf[i].feature1).size(); ++j) {
+                float x = ts.getValuesByName(cf[i].feature1)[j];
+                float y = ts.getValuesByName(cf[i].feature2)[j];
+                Point *p = new Point(x, y);
+                if (dev(*p, cf[i].lin_reg) > cf[i].threshold) {
+                    string s = cf[i].feature1 + '-' + cf[i].feature2;
+                    reports.push_back(AnomalyReport(s, ts.getData2()[0].getValues()[j]));
+                }
+            }
+        }
+  //  }
+    return reports;
+}
---
+++ backup/timeseries.cpp
@@ -0,0 +1,64 @@
+//
+// Created by avi on 06/11/2021.
+//
+
+#include "timeseries.h"
+
+void TimeSeries::initializeData(string fileName) {
+    int rows = 0;
+    int columns = 0;
+    std::ifstream file;
+    file.open(fileName);
+    string line;
+    while (getline(file, line)) {
+        if (rows == 1) {
+            columns = std::count(line.begin(), line.end(), ',');
+        }
+        string segment = line;
+        ++rows;
+    }
+    //columns = std::count(line.begin(), line.end(), ',');
+    ++columns;
+
+    data = new string *[rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new string[columns];
+    }
+}
+
+void TimeSeries::loadCSV(string fileName) {
+    initializeData(fileName);
+    std::ifstream file;
+    file.open(fileName);
+    string line;
+    for (int i = 0; getline(file, line); ++i) {
+        string segment = line;
+        std::stringstream string(line);
+        for (int j = 0; getline(string, segment, ','); ++j) {
+            if (i == 0) {
+                Feature *feature = new Feature(segment);
+                vec.push_back(*feature);
+            } else {
+                vec[j].addValue(std::stof(segment));
+            }
+        }
+    }
+}
+
+TimeSeries::TimeSeries(const char* CSVfileName) {
+    this->csv = CSVfileName;
+    loadCSV(csv);
+}
+
+string **TimeSeries::getData() {
+    return this->data;
+}
+std::vector<Feature> TimeSeries::getData2() const {
+    return this->vec;
+}
+vector<float> TimeSeries::getValuesByName(string name) const {
+    for (Feature i: vec) {
+        if (i.getName() == name)
+            return i.getValues();
+    }
+}
